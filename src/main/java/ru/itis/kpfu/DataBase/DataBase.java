5b0d8c99f66bdfd2b7d17743b17d27ba1d1e52f2
Ильяс Муллахметов <IIMullahmetov@gmail.com>
2016-09-29 21:51:01 +0300

    Initial commit

---
+++ src/main/java/ru/itis/kpfu/DataBase/DataBase.java
@@ -0,0 +1,8 @@
+package ru.itis.kpfu.DataBase;
+
+/**
+ * Created by IIMullahmetov on 9/29/2016.
+ */
+public class DataBase {
+    
+}
---
+++ src/main/java/ru/itis/kpfu/Model/Model.java
@@ -0,0 +1,7 @@
+package ru.itis.kpfu.Model;
+
+/**
+ * Created by IIMullahmetov on 9/29/2016.
+ */
+public class Model {
+}
